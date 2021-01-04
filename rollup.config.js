3a43d65b7656ed235b26129130247dede7247aee
Max <maxicespedes7@hotmail.com>
2021-01-04 20:08:12 +0100

    NO ANADA

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,29 @@
 import babel from 'rollup-plugin-babel';
+import external from 'rollup-plugin-peer-deps-external';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
 
 export default [
   {
     input: 'src/index.js',
     output: {
       name: 'system',
-      file: 'dist/system.js',
-      format: 'iife',
-      globals: 'react',
-    },
-    output: {
       dir: 'dist',
+      format: 'cjs',
       globals: {
-        react: 'React',
+        react: 'react',
+        '@emotion/styled': 'styled',
+        '@emotion/styled/base': '_styled',
+        '@emotion/core': 'core',
       },
     },
-    external: ['react'],
     plugins: [
       babel({
         exclude: 'node_modules/**',
       }),
+      external(),
+      nodeResolve(),
+      commonjs(),
     ],
   },
 ];
